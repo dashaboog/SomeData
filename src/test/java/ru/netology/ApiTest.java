5b7273db246e51962594214614c999c5d35fb3bb
Neto study <daria.bugaevskaya@gmail.com>
2023-03-12 19:54:43 +0300

    initial commit

---
+++ src/test/java/ru/netology/ApiTest.java
@@ -0,0 +1,18 @@
+package ru.netology.SomeData;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+class ApiTest {
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200)
+  .body(/* --> ваша проверка здесь <-- */)
+            ;
+}
