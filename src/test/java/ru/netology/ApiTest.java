2ea6cc76bfdc471cbe6715ce1fd3a9cc56fdc411
Neto study <daria.bugaevskaya@gmail.com>
2023-03-12 20:14:28 +0300

    tests executed

--- src/test/java/ru/netology/ApiTest.java
+++ src/test/java/ru/netology/ApiTest.java
@@ -1,18 +1,22 @@
-package ru.netology.SomeData;
+package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 class ApiTest {
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
-.when()
-  .post("/post")
-// Проверки
-.then()
-  .statusCode(200)
-  .body(/* --> ваша проверка здесь <-- */)
-            ;
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
 }
